fe37c0e8506b1414e4034b718de231c39d56cc0c
舒晨钰 <szyt_825@163.com>
2018-07-03 14:40:38 +0800

    开始干活啦
    从现在开始

---
+++ YT_iOSHelp/YT_iOSHelp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YT_iOSHelp
+//
+//  Created by yt on 2018/7/3.
+//  Copyright © 2018年 SamShu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YT_iOSHelp/YT_iOSHelp/UIKit/YT_UILable_VC.m
@@ -0,0 +1,53 @@
+//
+//  YT_UILable_VC.m
+//  YT_iOSHelp
+//
+//  Created by yt on 2018/7/3.
+//  Copyright © 2018年 SamShu. All rights reserved.
+//
+
+#import "YT_UILable_VC.h"
+
+@interface YT_UILable_VC ()
+
+@end
+
+@implementation YT_UILable_VC
+
+#pragma mark -视图 加载完成 即将显示 已经显示 即将消失 已经消失
+//视图已经加载完成
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+//根视图即将显⽰示
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:YES];
+}
+
+//根视图已经显⽰示
+-(void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:YES];
+}
+
+//根视图即将消失
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [super viewWillDisappear:YES];
+}
+
+//根视图已经消失
+- (void)viewDidDisappear:(BOOL)animated
+{
+    [super viewDidDisappear:YES];
+}
+//收到内存警告
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
